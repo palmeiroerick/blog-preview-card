edd4c3856dd363486f3587570e3774c0e34b990c
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-13 16:36:57 -0300

    feat: add Illustration component

---
+++ components/Illustration/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Illustration = () => {
+  return (
+    <Image
+      className="rounded-xl"
+      src="/assets/illustration-article.svg"
+      alt=""
+      width={312}
+      height={186.64}
+    />
+  );
+};
+
+export default Illustration;
