8930a70dbf483bc9e19345a3e830d500f671d357
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-13 16:37:20 -0300

    feat: add Learning component

---
+++ components/Learning/index.tsx
@@ -0,0 +1,9 @@
+const Learning = () => {
+  return (
+    <div className="flex h-7 w-20 items-center justify-center rounded-md bg-yellow text-sm font-extrabold">
+      Learning
+    </div>
+  );
+};
+
+export default Learning;
