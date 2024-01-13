9171fafff67e86991f56ab82dd6bc22b64a08124
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-13 16:38:19 -0300

    feat: add Text component

---
+++ components/Text/index.tsx
@@ -0,0 +1,10 @@
+const Text = () => {
+  return (
+    <p className="text-[13px] text-grey">
+      These languages are the backbone of every website, defining structure,
+      content, and presentation.
+    </p>
+  );
+};
+
+export default Text;
