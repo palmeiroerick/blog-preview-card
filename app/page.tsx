7cfdbacb15531ee5bfe85a6e3d035e7776424318
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-13 16:35:12 -0300

    style: add styles to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import Author from "@/components/Author";
+import Date from "@/components/Date";
+import Illustration from "@/components/Illustration";
+import Learning from "@/components/Learning";
+import Text from "@/components/Text";
+import Title from "@/components/Title";
+
 const Home = () => {
-  return <main>hello</main>;
+  return (
+    <main className="flex w-80 flex-col gap-4 rounded-2xl border bg-white p-7 shadow-[8px_8px_0_0_rgba(0,0,0)]">
+      <Illustration />
+      <Learning />
+      <Date />
+      <Title />
+      <Text />
+      <Author />
+    </main>
+  );
 };
 
 export default Home;
